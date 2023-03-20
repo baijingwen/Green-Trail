46fa73eb876b88a4ff1ad26a7224c76518b564bd
chelsealyl <106786054+chelsealyl@users.noreply.github.com>
2023-03-20 22:44:37 +1100

    sign up 1

--- app/src/main/java/com/example/myapplication/Signup.java
+++ app/src/main/java/com/example/myapplication/Signup.java
@@ -2,10 +2,14 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class Signup extends AppCompatActivity {
+public class Signup extends AppCompatActivity implements View.OnClickListener {
+
+    private Button nextButton, loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,28 @@ public class Signup extends AppCompatActivity {
 
         //next to signup2
         //login to main
+        Button nextButton = findViewById(R.id.nextBtn);
+        Button loginButton = findViewById(R.id.loginBtn);
+        nextButton.setOnClickListener(this);
+        loginButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.loginBtn:
+                startActivity(new Intent(Signup.this, LoginActivity.class));
+                break;
+            case R.id.nextBtn:
+
+                startActivity(new Intent(Signup.this, Signup2.class));
+                break;
+        }
+    }
+
+    private void transfer() {
 
     }
 }
+
